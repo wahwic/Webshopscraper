49212f6166ad46d528573a654ed4f9b269747133
Samuel Kucsera <s.kucsera@gmail.com>
2021-01-23 18:53:17 +0100

    first working version

--- webshopscraper.py
+++ webshopscraper.py
@@ -15,28 +15,37 @@ options.binary_location = "C:/Program Files (x86)/Google/Chrome/Application/chro
 options = webdriver.ChromeOptions()
 options.headless = True
 driverpath = "C:/Users/skucs/Documents/programming/Python/chromedriver.exe"
-driver = webdriver.Chrome(chrome_options=options, executable_path=driverpath)
+driver = webdriver.Chrome(options=options, executable_path=driverpath)
 XMLroot = ET.parse('shops.xml', ET.XMLParser(encoding="utf-8")).getroot()
 
-#get Alza content
-def Alzascrape():
-    try:
-        searchContent = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME, "browsingitemcontainer")))
-        items = searchContent.find_elements_by_css_selector('div.browsingitem')
-        for item in items:
-            name = item.find_element_by_css_selector('div.fb>a.name')
-            try:
-                price = item.find_element_by_css_selector('div.priceInner>span.c2')
-                price = price.text.split(' ')[0]
-                price = float(price.replace(',','.'))
-            except NoSuchElementException:
-                price = "Out of stock"
-            print('Alza: '+ name.text.split('\n')[0] + ' -> ',price,'{currency}'.format(currency='€!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!' if type(price) == type(1.1) else ''))
-    finally:
-        driver.quit()
+def getShop(XMLshopname):
+    XMLsearchContent = XMLroot.find(".//shop[@name='{0}']/searchContent".format(XMLshopname)).text
+    XMLitems = XMLroot.find(".//shop[@name='{0}']/items".format(XMLshopname)).text
+    XMLname = XMLroot.find(".//shop[@name='{0}']/ProductName".format(XMLshopname)).text
+    XMLprice = XMLroot.find(".//shop[@name='{0}']/ProductPrice".format(XMLshopname)).text
+    getNameAndPrice(XMLshopname, XMLsearchContent, XMLitems, XMLname, XMLprice)
+
+def getNameAndPrice(XMLshopname, XMLsearchContent, XMLitems, XMLname, XMLprice):
+    searchContent = WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CSS_SELECTOR, XMLsearchContent)))
+    items = searchContent.find_elements_by_css_selector(XMLitems)
+    for item in items:
+        name = item.find_element_by_css_selector(XMLname)
+        try:
+            price = item.find_element_by_css_selector(XMLprice)
+            price = price.text.split(' ')[0]
+            price = float(price.replace(',','.'))
+        except NoSuchElementException:
+            price = "Out of stock"
+        print(XMLshopname +': '+ name.text.split('\n')[0] + ' -> ',price,'{currency}'.format(currency='€!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!' if type(price) == type(1.1) else ''))
 
 #open page
-for XMLurl in XMLroot.findall('./shop/url'):
-    driver.get(XMLurl.text)
+for XMLshop in XMLroot.findall('./shop'):
+    XMLurl = XMLshop.find('./url').text
+    XMLshopname = XMLshop.find('./shopname').text
+    driver.get(XMLurl)
     time.sleep(5)
-    Alzascrape()
+    try:
+        getShop(XMLshopname)
+    except:
+        print("That's a no.")
+driver.quit()
